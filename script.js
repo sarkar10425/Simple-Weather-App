aaaa7502d9cfa5ab607baca7da68216ac9ab2672
Arindam Sarkar <43996902+sarkar10425@users.noreply.github.com>
2020-05-20 12:58:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-/**
- * Weather App
- * DONE: Complete getWeatherData() to return json response Promise
- * DONE: Complete searchCity() to get user input and get data using getWeatherData()
- * DONE: Complete showWeatherData() to set the data in the the html file from response
- */
 
-// API_KEY for maps api
 let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
 
-/**
- * Retrieve weather data from openweathermap
- * HINT: Use fetch()
- * HINT: URL should look like this: 
- * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
- */
+
 getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
   //HINT: Use template literals to create a url with input and an API key
@@ -28,10 +16,6 @@ getWeatherData = (city) => {
 }
 
 
-/**
- * Retrieve city input and get the weather data
- * HINT: Use the promise returned from getWeatherData()
- */
 searchCity = () => {
   const city = document.getElementById('city-input').value;
   // CODE GOES HERE
@@ -44,16 +28,13 @@ searchCity = () => {
 
 }
 
-/**
- * Show the weather data in HTML
- * HINT: make sure to console log the weatherData to see how the data looks like
- */
+
 showWeatherData = (weatherData) => {
   //CODE GOES HERE
   document.getElementById("city-name").innerHTML = weatherData.name;
-  document.getElementById("temp").innerHTML = weatherData.main.temp;
-  document.getElementById("min-temp").innerHTML = weatherData.main.temp_min;
-  document.getElementById("max-temp").innerHTML = weatherData.main.temp_max;
+  document.getElementById("temp").innerHTML = ((weatherData.main.temp-32)*(5*1.0/9)).toFixed(2);
+  document.getElementById("min-temp").innerHTML = ((weatherData.main.temp_min-32)*(5*1.0/9)).toFixed(2);
+  document.getElementById("max-temp").innerHTML = ((weatherData.main.temp_max-32)*(5*1.0/9)).toFixed(2);
   document.getElementById("weather-type").innerHTML = weatherData.weather[0].main;
   
 }
